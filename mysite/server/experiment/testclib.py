fd0fdfaa60c28b1a172efa62f28a0444d8207b59
Lenvia <yrp1199@163.com>
2023-05-31 17:56:39 +0800

    feat: gen picture using dylib

--- mysite/server/experiment/testclib.py
+++ mysite/server/experiment/testclib.py
@@ -5,11 +5,27 @@
 """
 import ctypes
 
+# # 加载库
+# lib = ctypes.cdll.LoadLibrary('/Users/yy/CLionProjects/dylib_demo/cmake-build-debug/libaaaaaa.dylib')
+#
+# # 调用函数
+# say_hello = lib.sayHello
+# say_hello.restype = None
+# say_hello.argtypes = []
+# say_hello()
+
 # 加载库
-lib = ctypes.cdll.LoadLibrary('/Users/yy/CLionProjects/dylib_demo/cmake-build-debug/libaaaaaa.dylib')
+lib = ctypes.cdll.LoadLibrary('/Users/yy/CLionProjects/vtkm-flow/cmake-build-debug/libstreamline.dylib')
+
+file_name = "/Users/yy/GithubProjects/GPT-flow-vis/mysite/server/experiment/data/streamlines/IWP_DAILY_20141123_531_1621.vtk"
+width = 780
+height = 480
+output = "/Users/yy/GithubProjects/GPT-flow-vis/mysite/server/experiment/111.png"
+
 
 # 调用函数
-say_hello = lib.sayHello
-say_hello.restype = None
-say_hello.argtypes = []
-say_hello()
+gen = lib.gen
+gen.restype = None
+gen.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_char_p]
+gen(file_name.encode('utf-8'), width, height, output.encode('utf-8'))
+
