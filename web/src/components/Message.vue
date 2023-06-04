ce85d617d5e6b1d5e5286d47a1f957e97eb22543
Lenvia <yrp1199@163.com>
2023-06-04 17:44:26 +0800

    feat: add message component

---
+++ web/src/components/Message.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="message" :class="`message-${sender}`">
+    <div class="message-content">{{ content }}</div>
+    <div class="message-sender">{{ sender }}</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Message',
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+    sender: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+.message {
+  margin: 5px;
+  padding: 5px;
+  border-radius: 5px;
+}
+.message-content {
+  font-size: 14px;
+}
+.message-sender {
+  font-size: 10px;
+  color: gray;
+}
+.message-me {
+  background-color: #00cc00;
+  color: white;
+}
+.message-system {
+  background-color: #E4E7ED;
+  color: black;
+}
+
+
+</style>
