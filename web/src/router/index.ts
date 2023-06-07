ce93fe51e4e479fd8a62f53e4dbfb21835d7fc33
Lenvia <yrp1199@163.com>
2023-06-07 13:37:53 +0800

    fix: reset router

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import TestView from "@/views/TestView.vue";
 import MainView from "@/views/MainView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'main',
+    component: MainView
   },
 
   {
